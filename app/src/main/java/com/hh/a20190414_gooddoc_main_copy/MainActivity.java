12826da3ca42f54d83b379b60c7224e67fb55444
babobungmin <babobungmin@naver.com>
2019-04-14 15:03:35 +0900

    메인화면만들기

--- app/src/main/java/com/hh/a20190414_gooddoc_main_copy/MainActivity.java
+++ app/src/main/java/com/hh/a20190414_gooddoc_main_copy/MainActivity.java
@@ -2,12 +2,36 @@ package com.hh.a20190414_gooddoc_main_copy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtSearch;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtSearch = findViewById(R.id.edtSearch);
+        btnSearch = findViewById(R.id.btnSearch);
+        
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String edtText = edtSearch.getText().toString();
+                String formatEdtText = String.format("[%s]", edtText);
+
+
+                Toast.makeText(MainActivity.this, formatEdtText, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
